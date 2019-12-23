82cc364cb0991f16718641f250ba7680f559dd27
Abudu Abidun Sulaiman <sulaimanabiodun172@gmail.com>
2019-12-23 10:43:46 +0100

    setup redux with simple snippet of code (#2)
    Merge setup-redux to master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import style from './style.scss';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction';
 
+
+const mapStateToProps = state => ({
+    ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+    simpleAction: () => dispatch(simpleAction())
+})
 class App extends Component {
+    simpleAction = (event) => {
+        this.props.simpleAction();
+    }
     render(){
         return (
             <div className="App">
                 <h1> Welcome to Edugate !!!</h1>
+                <button onClick={this.simpleAction}>Test redux action</button>
+                <pre>
+                    {
+                        JSON.stringify(this.props)
+                    }
+                </pre>
             </div>
         );
     }
 }
 
-export default hot(module)(App);
+export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(App));
+
---
+++ src/components/signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function home() {
+    return (
+        <div className="App">
+            <h1>Signup !!!</h1>
+            <p>Signup page</p>
+        </div>
+    );
+}
