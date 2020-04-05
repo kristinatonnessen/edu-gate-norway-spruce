adbeaa90f333790625b89aaed7c3470ed80db40c
sulenchy <sulaimanabiodun172@gmail.com>
2020-04-05 17:24:48 +0100

    fix issue with routing when one changes the url in the browsers address bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
