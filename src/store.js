d7d1cdaba86f0c0846ec31fdc4352312a7a54c2f
sulenchy <sulaimanabiodun172@gmail.com>
2020-07-04 18:34:13 +0100

    add redux logger

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist'
+import { createLogger } from 'redux-logger';
 import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+
+const logger = createLogger();
 
 
 export const initialState = {
@@ -23,7 +27,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const store =  createStore(
   persistedReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 export const persistor = persistStore(store);
